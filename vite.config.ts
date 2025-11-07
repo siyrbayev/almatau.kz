b2a92f1c8c8e2ada99c9b1396ccce251a4a4057a
siyrbayev <nurym.kazah@gmail.com>
2025-11-07 21:05:41 +0500

    fix: add vite_ prefix for env variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      // перенаправляем /api на vercel dev (3000)
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-      },
-    },
-  },
-  preview: { port: 5174 },
-});
+  build: {
+    outDir: 'dist'
+  }
+})
