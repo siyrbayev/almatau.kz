f71c59a00c315d957f3e2d5b64f98000ecfb754b
siyrbayev <nurym.kazah@gmail.com>
2025-11-08 20:45:16 +0500

    fix: add vite_ prefix for env variables

--- src/App.jsx
+++ src/App.jsx
@@ -678,7 +678,7 @@ function SearchPage(){
               </div>
               <div className="title">{it.title||it.name}</div>
               <div className="badge">Штрихкод: {it.barcode}</div>
-              <div className="badge">Цена: <h2 style={{color: totalBalance(it)<0 ? 'green' : 'inherit'}} >{fmtMoney(it.selling_price ?? it.price)}</p></div>
+              <div className="badge">Цена: <h2 style={{color: totalBalance(it)<0 ? 'green' : 'inherit'}}>{fmtMoney(it.selling_price ?? it.price)}</h2></div>
               <div className="badge" >Остаток: <p style={{color: totalBalance(it)<0 ? 'green' : 'inherit'}}>{totalBalance(it)<0 ? "Тауардың бар-жоғын сұраңыз" : totalBalance(it)} {it.unit_name}</p></div>
             </div>
           ))}
